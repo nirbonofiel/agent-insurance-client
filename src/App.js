462749fedae31d7634b02f36e8a82fd217cac2ea
Nir Bonofiel <nirbono10@gmail.com>
2020-08-23 14:23:28 +0300

    install redux, redirct '/' to insuranceForAgent

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './App.scss';
 import Home from './containers/home/Home'
 import Switch from 'react-bootstrap/esm/Switch';
-import { Route } from 'react-router-dom';
+import { Route, Redirect} from 'react-router-dom';
 import Agent from './components/agent/Agent';
 import Insurance from './components/insurance/Insurace';
 import AgentInsurance from './components/agentInsurance/AgentInsurance'
@@ -16,6 +16,7 @@ function App() {
           <Route path='/insuranceForAgent' component={AgentInsurance} />
           <Route path='/agent' component={Agent}/>
           <Route path='/insurance' component={Insurance} />
+          <Redirect exact from={'/'} to={'/insuranceForAgent'} />
         </Switch>
       </Home>
     </div>
